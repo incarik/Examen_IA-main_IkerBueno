0c6122058fb32437b23b80bcb77d2190d8cc951c
incarik <incarik1@gmail.com>
2025-02-24 11:54:59 +0100

    ia funcional

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public enum EnemyState
+    {
+        Patrolling,
+        Searching,
+        Chasing,
+        Waiting,
+        Attacking
+    }
+
+    public EnemyState currentState;
+
+    [SerializeField] private Transform[]_patrolPoints;
+    private int _patrolIndex;
+    private Transform _playerTransform;
+    [SerializeField] private float _visionRange = 15;
+    [SerializeField] private float _attackRange = 3;
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Start()
+    {
+        currentState = EnemyState.Patrolling;
+        SetPatrolPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            
+            case EnemyState.Patrolling:
+                Patrol();
+            break;
+            case EnemyState.Searching:
+                Search();
+            break;
+            case EnemyState.Chasing:
+                Chase();
+            break;
+            case EnemyState.Waiting:
+                Wait();
+            break;
+            case EnemyState.Attacking:
+                Attack();
+            break;
+        }
+
+    }
+
+    void Patrol()
+    {
+      
+        if(InRange(_visionRange))
+        {
+            currentState = EnemyState.Chasing;
+        }
+        
+        if(_agent.remainingDistance < 0.5f)
+        {
+            SetPatrolPoint();
+        }
+    }
+
+    void SetPatrolPoint()
+    {
+        _agent.destination = _patrolPoints[Random.Range(0, _patrolPoints.Length)].position; //busca un punto aleatorio
+    }
+
+    void Search()
+    {
+
+    }
+
+    void Chase()
+    {
+        
+
+        if(!InRange(_visionRange))
+        {
+            currentState = EnemyState.Chasing;
+        }
+
+        if(InRange(_attackRange))
+        {
+            currentState = EnemyState.Attacking;
+        }
+
+        _agent.destination = _playerTransform.position;
+    }
+
+    bool InRange(float range)
+    {
+        return Vector3.Distance(transform.position, _playerTransform.position) < range;
+    }
+
+    void Wait()
+    {
+
+    }
+
+    void Attack()
+    {
+        Debug.Log("atacando");
+
+        currentState = EnemyState.Searching;
+    }
+}
